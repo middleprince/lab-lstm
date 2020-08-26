f2597ede2c293adc7a6d29ee4c5de01846d3b3c5
middleprince <hellangel.ah@gmail.com>
2020-08-26 15:20:08 +0800

    doc: Add some comments

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 # -*- coding: UTF-8 -*-
-"""
-@author: hichenway
-@contact: lyshello123@163.com
-@time: 2020/5/9 17:00
-@license: Apache
-主程序：包括配置，数据读取，日志记录，绘图，模型训练和预测
-"""
 
 import pandas as pd
 import numpy as np
@@ -15,55 +8,43 @@ import time
 import logging
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
-frame = "pytorch"  # 可选： "keras", "pytorch", "tensorflow"
-if frame == "pytorch":
-    from model.model_pytorch import train, predict
-elif frame == "keras":
-    from model.model_keras import train, predict
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
-elif frame == "tensorflow":
-    from model.model_tensorflow import train, predict
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'    # tf和keras下会有很多tf的warning，但不影响训练
-else:
-    raise Exception("Wrong frame seletion")
+from model.model_pytorch import train, predict
 
 class Config:
     # 数据参数
-    feature_columns = list(range(2, 4))     # 要作为feature的列，按原数据从0开始计算，也可以用list 如 [2,4,6,8] 设置
-    label_columns = [2, 3]                  # 要预测的列，按原数据从0开始计算, 如同时预测第四，五列 最低价和最高价
-    # label_in_feature_index = [feature_columns.index(i) for i in label_columns]  # 这样写不行
-    label_in_feature_index = (lambda x,y: [x.index(i) for i in y])(feature_columns, label_columns)  # 因为feature不一定从0开始
+    feature_columns = list(range(2, 4))     # 要作为feature的列，按原数据从0开始计算
+    label_columns = [2, 3]                  # 要预测的列，也即是label data按原数据从0开始计算 
+    label_in_feature_index = (lambda x,y: [x.index(i) for i in y])(feature_columns, label_columns)  # 因为feature含有label的index
 
-    predict_day = 1             # 预测未来几天
+    predict_length = 10             # 预测序列的长度
 
     # 网络参数
     input_size = len(feature_columns)
     output_size = len(label_columns)
 
     hidden_size = 128           # LSTM的隐藏层大小，也是输出大小
-    lstm_layers = 3             # LSTM的堆叠层数
+    lstm_layers = 2             # LSTM的堆叠层数
     dropout_rate = 0.2          # dropout概率
-    time_step = 14              # 这个参数很重要，是设置用前多少天的数据来预测，也是LSTM的time step数
+    time_step = 14              # LSTM的time step 序列长度
 
     # 训练参数
     do_train = True
-    do_predict = True
+    do_predict = False
     add_train = False           # 是否载入已有模型参数进行增量训练
     shuffle_train_data = False   # 是否对训练数据做shuffle
-    use_cuda = False            # 是否使用GPU训练
+    use_cuda = True            # 是否使用GPU训练
 
-    train_data_rate = 0.95      # 训练数据占总体数据比例，测试数据就是 1-train_data_rate
+    train_data_rate = 0.90      # 训练数据占总体数据比例，测试数据就是 1-train_data_rate
     valid_data_rate = 0.15      # 验证数据占训练数据比例，验证集在训练过程使用，为了做模型和参数选择
 
     batch_size = 32
     learning_rate = 0.001
-    epoch = 100                  # 整个训练集被训练多少遍，不考虑早停的前提下
+    epoch = 150                  # 整个训练集被训练多少遍，不考虑早停的前提下
     patience = 40                # 训练多少epoch，验证集没提升就停掉
     random_seed = 42            # 随机种子，保证可复现
 
-    do_continue_train = True    # 每次训练把上一次的final_state作为下一次的init_state，仅用于RNN类型模型，目前仅支持pytorch
-    continue_flag = ""           # 但实际效果不佳，可能原因：仅能以 batch_size = 1 训练
+    do_continue_train = True    # 每次训练把上一次的final_state作为下一次的init_state,使用道间相似性 
+    continue_flag = ""           # TODO.但实际效果不佳，可能原因：仅能以 batch_size = 1 训练
     if do_continue_train:
         shuffle_train_data = False
         batch_size = 1
@@ -74,15 +55,13 @@ class Config:
     debug_num = 500  # 仅用debug_num条数据来调试
 
     # 框架参数
-    used_frame = frame  # 选择的深度学习框架，不同的框架模型保存后缀不一样
-    model_postfix = {"pytorch": ".pth", "keras": ".h5", "tensorflow": ".ckpt"}
-    model_name = "model_" + continue_flag + used_frame + model_postfix[used_frame]
+    model_name = "model_" + continue_flag 
 
     # 路径参数
     cur_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
-    train_data_path = "./data/velocity-single.csv"
-    #model_save_path = "./checkpoint/" + used_frame + cur_time + '_' + "/"
-    model_save_path = "./checkpoint/" + used_frame + "/"
+    train_data_path = "./data/large.csv"
+    #model_save_path = "./checkpoint/" + cur_time + '_' + "/"
+    model_save_path = "./checkpoint/" + "/"
     figure_save_path = "./figure/"
     log_save_path = "./log/"
     do_log_save = True                  # 是否将config和训练过程记录到log
@@ -94,7 +73,7 @@ class Config:
         os.mkdir(figure_save_path)
     if do_train and (do_log_save or do_train_visualized):
         cur_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
-        log_save_path = log_save_path + cur_time + '_' + used_frame + "/"
+        log_save_path = log_save_path + cur_time + '_' + "/"
         os.makedirs(log_save_path)
 
 
@@ -110,7 +89,7 @@ class Data:
         self.std = np.std(self.data, axis=0)
         self.norm_data = (self.data - self.mean)/self.std   # 归一化，去量纲
 
-        self.start_num_in_test = 0      # 测试集中前几天的数据会被删掉，因为它不够一个time_step
+        self.start_num_in_test = 0      # 测试集中前t个time-step数据会被删掉，因为它不够一个time_step
 
     def read_data(self):                # 读取初始数据
         if self.config.debug_mode:
@@ -122,18 +101,17 @@ class Data:
 
     def get_train_and_valid_data(self):
         feature_data = self.norm_data[:self.train_num]
-        label_data = self.norm_data[self.config.predict_day : self.config.predict_day + self.train_num,
-                                    self.config.label_in_feature_index]    # 将延后几天的数据作为label
+        label_data = self.norm_data[self.config.predict_length : self.config.predict_length + self.train_num,
+                self.config.label_in_feature_index]    # TODO:将input与predicted错位，使用语言模型的设计:label是与input错开predict_length time-sequence此设计可能需要调整
 
         if not self.config.do_continue_train:
-            # 在非连续训练模式下，每time_step行数据会作为一个样本，两个样本错开一行，比如：1-20行，2-21行。。。。
+            # 在非连续训练模式下，每time_step行数据会作为一个样本，两个样本错开一行，eg.：1-20行，2-21行。。。。
             train_x = [feature_data[i:i+self.config.time_step] for i in range(self.train_num-self.config.time_step)]
             train_y = [label_data[i:i+self.config.time_step] for i in range(self.train_num-self.config.time_step)]
         else:
-            # 在连续训练模式下，每time_step行数据会作为一个样本，两个样本错开time_step行，
+            # TODO:在连续训练模式下，每time_step行数据会作为一个样本，两个样本错开time_step行，
             # 比如：1-20行，21-40行。。。到数据末尾，然后又是 2-21行，22-41行。。。到数据末尾，……
             # 这样才可以把上一个样本的final_state作为下一个样本的init_state，而且不能shuffle
-            # 目前本项目中仅能在pytorch的RNN系列模型中用
             train_x = [feature_data[start_index + i*self.config.time_step : start_index + (i+1)*self.config.time_step]
                        for start_index in range(self.config.time_step)
                        for i in range((self.train_num - start_index) // self.config.time_step)]
@@ -204,30 +182,30 @@ def draw(config: Config, origin_data: Data, logger, predict_norm_data: np.ndarra
     label_name = [origin_data.data_column_name[i] for i in config.label_in_feature_index]
     label_column_num = len(config.label_columns)
 
-    # label 和 predict 是错开config.predict_day天的数据的
+    # label 和 predict 是错开config.predict_length天的数据的
     # 下面是两种norm后的loss的计算方式，结果是一样的，可以简单手推一下
     # label_norm_data = origin_data.norm_data[origin_data.train_num + origin_data.start_num_in_test:,
     #              config.label_in_feature_index]
-    # loss_norm = np.mean((label_norm_data[config.predict_day:] - predict_norm_data[:-config.predict_day]) ** 2, axis=0)
+    # loss_norm = np.mean((label_norm_data[config.predict_length:] - predict_norm_data[:-config.predict_length]) ** 2, axis=0)
     # logger.info("The mean squared error of stock {} is ".format(label_name) + str(loss_norm))
 
-    loss = np.mean((label_data[config.predict_day:] - predict_data[:-config.predict_day] ) ** 2, axis=0)
+    loss = np.mean((label_data[config.predict_length:] - predict_data[:-config.predict_length] ) ** 2, axis=0)
     loss_norm = loss/(origin_data.std[config.label_in_feature_index] ** 2)
     logger.info("The mean squared error of stock {} is ".format(label_name) + str(loss_norm))
 
     label_X = range(origin_data.data_num - origin_data.train_num - origin_data.start_num_in_test)
-    predict_X = [ x + config.predict_day for x in label_X]
+    predict_X = [ x + config.predict_length for x in label_X]
 
     if not sys.platform.startswith('linux'):    # 无桌面的Linux下无法输出，如果是有桌面的Linux，如Ubuntu，可去掉这一行
         for i in range(label_column_num):
             plt.figure(i+1)                     # 预测数据绘制
             plt.plot(label_X, label_data[:, i], label='label')
             plt.plot(predict_X, predict_data[:, i], label='predict')
-            plt.title("Predict stock {} price with {}".format(label_name[i], config.used_frame))
-            logger.info("The predicted stock {} for the next {} day(s) is: ".format(label_name[i], config.predict_day) +
-                  str(np.squeeze(predict_data[-config.predict_day:, i])))
+            plt.title("Predict T-V {} ".format(label_name[i]))
+            logger.info("The predicted stock {} for the next {} day(s) is: ".format(label_name[i], config.predict_length) +
+                  str(np.squeeze(predict_data[-config.predict_length:, i])))
             if config.do_figure_save:
-                plt.savefig(config.figure_save_path+"{}predict_{}_with_{}.png".format(config.continue_flag, label_name[i], config.used_frame))
+                plt.savefig(config.figure_save_path+"{}_predict_{}.png".format(config.continue_flag, label_name[i]))
 
         plt.show()
 
@@ -253,10 +231,10 @@ if __name__=="__main__":
     import argparse
     # argparse方便于命令行下输入参数，可以根据需要增加更多
     parser = argparse.ArgumentParser()
-    # parser.add_argument("-t", "--do_train", default=False, type=bool, help="whether to train")
-    # parser.add_argument("-p", "--do_predict", default=True, type=bool, help="whether to train")
-    # parser.add_argument("-b", "--batch_size", default=64, type=int, help="batch size")
-    # parser.add_argument("-e", "--epoch", default=20, type=int, help="epochs num")
+    parser.add_argument("-t", "--do_train", default=True, type=bool, help="whether to train")
+    parser.add_argument("-p", "--do_predict", default=False, type=bool, help="whether to train")
+    parser.add_argument("-b", "--batch_size", default=64, type=int, help="batch size")
+    parser.add_argument("-e", "--epoch", default=50, type=int, help="epochs num")
     args = parser.parse_args()
 
     con = Config()
